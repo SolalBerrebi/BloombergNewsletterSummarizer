079b810efd8f694f0b6a65f8b1e25214d5a4c97e
SolalBerrebi <76755511+SolalBerrebi@users.noreply.github.com>
2024-09-26 16:30:17 +0200

    Update getchannelIDTelegram_vgit.py

--- scripts/getchannelIDTelegram_vgit.py
+++ scripts/getchannelIDTelegram_vgit.py
@@ -9,7 +9,7 @@ Created on Thu Sep 26 11:42:56 2024
 import requests
 
 # Replace with your bot's token
-TOKEN = '7503985165:AAHAfzkeFaC_qkfGriZxCpzCPcAmR6ndolo'
+TOKEN = 'your_token'
 
 # URL to get updates from the bot
 url = f'https://api.telegram.org/bot{TOKEN}/getUpdates'
